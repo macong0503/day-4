63c6f79a58c3abad2e693a01ead4e27c099c85b6
macong0503 <1512346520@qq.com>
2021-07-15 15:33:06 +0800

    Create TextArray.java

---
+++ TextArray.java
@@ -0,0 +1,12 @@
+package com.hqyj.mc.Text;
+
+public class TextArray {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,9,5,8,6,7,0};
+        ArrayPrint_1.arr(a);
+        for (int i = 0; i <a.length ; i++) {
+            System.out.print(a[i]+" ");
+        }
+
+    }
+}
